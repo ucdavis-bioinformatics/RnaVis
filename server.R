34d1106ca908951d4a7557c8c35edf0fb0237d07
Navya Kompella <navyakrose@gmail.com>
2017-07-11 12:40:56 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,16 +1,28 @@
 library(shiny)
 
 function(input, output) {
-  df <- read.table("/Users/Navya/Downloads/DavisData2.txt", header = TRUE, sep= "\t", as.is = TRUE)
-  output$mytable = renderDataTable({
-    df
+  df <- read.table("/Users/Navya/Desktop/DavisData2.txt", header = TRUE, sep= "\t", as.is = TRUE)
+  #df <- read.table("/Users/Navya/Downloads/DavisData10Rows.txt", header = TRUE, sep= "\t", as.is = TRUE)
+  sorted_data_table <- df[order( df[,2]) , ]
+  
+  output$mytable = renderDataTable({sorted_data_table})
+  output$GeneData <- renderPlot({ 
+    mydata2 <- mydata[,2-3]
+    barplot(mydata2[,input$mydatacol],
+            main = "Bar Plot",
+            ylab="data", 
+            xlab= "genes")
+    
   })
   output$distPlot <- renderPlot({
-    #create bins based on input$bins from ui.R
     logFC <- mydata[, 2]
     
     #draw the histogram with the specified number of bins
-    hist(logFC, breaks = input$bins, col = 'blue', border = 'white')
+    hist(logFC, breaks = 50, col = 'blue', border = 'white')
   })
+  
+ 
+
+
   
 }
