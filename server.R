9188b33cd2f913c1e8d19b995a34112f7d0a27b8
Navya Kompella <navyakrose@gmail.com>
2017-07-16 16:21:56 -0700

    Update server.R

--- server.R
+++ server.R
@@ -2,27 +2,31 @@ library(shiny)
 
 function(input, output) {
   df <- read.table("/Users/Navya/Desktop/DavisData2.txt", header = TRUE, sep= "\t", as.is = TRUE)
-  #df <- read.table("/Users/Navya/Downloads/DavisData10Rows.txt", header = TRUE, sep= "\t", as.is = TRUE)
   sorted_data_table <- df[order( df[,2]) , ]
   
-  output$mytable = renderDataTable({sorted_data_table})
+  
   output$GeneData <- renderPlot({ 
-    mydata2 <- mydata[,2-3]
-    barplot(mydata2[,input$mydatacol],
-            main = "Bar Plot",
-            ylab="data", 
-            xlab= "genes")
-    
+    mydata2 <- mydata[order(mydata$logFC),2-3]
+    mydata3 <-subset(mydata2, (logFC>input$min) & (logFC<input$max))
+    barplot(mydata3[,"logFC"],
+            main = "logFC barplot",
+            ylab="genes", 
+            xlab= "data",
+            horiz = TRUE)
   })
+  
+  output$mytable = renderDataTable({  
+    mydata2 <- mydata[order(mydata$logFC),]
+    #mydata2 <- mydata
+    mydata3 <-subset(mydata2, (logFC>input$min) & (logFC<input$max))
+    mydata3
+  })
+  #output$mytable = renderDataTable({sorted_data_table})
+  
   output$distPlot <- renderPlot({
     logFC <- mydata[, 2]
     
     #draw the histogram with the specified number of bins
     hist(logFC, breaks = 50, col = 'blue', border = 'white')
   })
-  
- 
-
-
-  
 }
