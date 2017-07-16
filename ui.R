ccb0483f1522f9e7e3d8b6e06a72bc4bd1d5bbdc
Navya Kompella <navyakrose@gmail.com>
2017-07-16 16:22:33 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,24 +1,28 @@
 library(shiny)
 
 shinyUI(fluidPage(
-
+  
+  
   titlePanel("Histogram of logFC"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("min", "Lower Value for Range ",
+                  choices = seq(min(mydata3[,2]), by = .25)),
+      selectInput("max", "Max value for range: ",
+                  choices = seq(max(mydata3[,2]), by = -.25)),
+      
+      helpText("Davis Data")
+    ),
     
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("mydatacol", "Columns: ",
-                    choices = colnames(mydata[,2-3])),
-        
-        helpText("Davis Data")
-      ),
-      
-      # Show a plot of the generated distribution
-      
-      mainPanel(
-        tabsetPanel(type = "tabs", 
-                    tabPanel("Data Table", dataTableOutput('mytable')),
-                    tabPanel("Histogram", plotOutput("distPlot")), 
-                    tabPanel("Bar Plot", plotOutput("GeneData")) 
+    # Show a plot of the generated distribution
+    
+    mainPanel(
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Data Table", dataTableOutput('mytable')),
+                  tabPanel("Histogram", plotOutput("distPlot")), 
+                  tabPanel("Bar Plot", plotOutput("GeneData"))
+                  
+      )
     )
-  )
-)))
+  )))
