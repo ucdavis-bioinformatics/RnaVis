332e4ef80751aab6da53172ad8c225d73829aa86
Navya Kompella <navyakrose@gmail.com>
2017-07-18 22:41:02 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,27 +1,28 @@
 library(shiny)
 
 function(input, output) {
-  df <- read.table("/Users/Navya/Desktop/DavisData2.txt", header = TRUE, sep= "\t", as.is = TRUE)
-  sorted_data_table <- df[order( df[,2]) , ]
   
+  mydata <- read.table("/Users/Navya/Desktop/DavisData2.txt", header = TRUE, sep= "\t", as.is = TRUE)
   
   output$GeneData <- renderPlot({ 
-    mydata2 <- mydata[order(mydata$logFC),2-3]
+    mydata2 <- mydata[order(mydata$logFC),]
     mydata3 <-subset(mydata2, (logFC>input$min) & (logFC<input$max))
+    cols <- c("blue", "lightgreen")[(mydata3[,2] > 0) +1]
     barplot(mydata3[,"logFC"],
             main = "logFC barplot",
+            col = cols,
+            border = cols,
             ylab="genes", 
             xlab= "data",
             horiz = TRUE)
+    
+  })
+  output$mytable <- renderDataTable({  
+    mydata2 <- mydata[order(mydata$logFC),]
+    mydata3 <-subset(mydata2, (logFC>input$min) & (logFC<input$max))
+    #DT::datatable(mydata3[, input$show_vars, drop = FALSE])
   })
   
-  output$mytable = renderDataTable({  
-    mydata2 <- mydata[order(mydata$logFC),]
-    #mydata2 <- mydata
-    mydata3 <-subset(mydata2, (logFC>input$min) & (logFC<input$max))
-    mydata3
-  })
-  #output$mytable = renderDataTable({sorted_data_table})
   
   output$distPlot <- renderPlot({
     logFC <- mydata[, 2]
@@ -29,4 +30,5 @@ function(input, output) {
     #draw the histogram with the specified number of bins
     hist(logFC, breaks = 50, col = 'blue', border = 'white')
   })
+  
 }
