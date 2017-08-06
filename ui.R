76763f2e4cd3103e3b1f64579b1c1b86a59d2c0a
Navya Kompella <navyakrose@gmail.com>
2017-08-06 15:04:04 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,34 +1,58 @@
 library(shiny)
+library(ggplot2)
 
-shinyUI(fluidPage(
-  
-  
-  titlePanel("Data Table"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("min", "Lower Value for Range ",
-                  choices = seq(min(mydata3[,2]), by =.25)),
-      selectInput("max", "Max value for range: ",
-                  choices = seq(max(mydata3[,2]), by = -.25)),
-      helpText("Davis Data:")
-      #conditionalPanel(
-        #'input.dataset === "mydata3"',
-        #checkboxGroupInput('show_vars', 'Columns to show:',
-                           #names(mydata3), selected = names(mydata3))
-        
-      #)
 
+fluidPage(
+  
+  fluidRow(
+    
+    column (width = 3,
+            
+            wellPanel(
+#***************************************************************************
+#Read the file and pass file handle "file1" to Server side
+#***************************************************************************
+              uiOutput("input_options0"),
+#***************************************************************************
+#Choices for minimum value, logFC
+#***************************************************************************
+              uiOutput("input_options_logFC1"),
+#***************************************************************************
+#***************************************************************************        
+              uiOutput("input_options_logFC2"),
+              uiOutput("input_options_logFC3"),
+              
+              
+#***************************************************************************
+#Select Max P.Value
+#***************************************************************************
+              uiOutput("input_options_pvalue1")
+              
+              
+            )
+            
+    ),
+#***************************************************************************
+#Output table and bar graph on different tabs of the browser
+#***************************************************************************
+    
+    column (width=5, 
+            tabsetPanel(
+              id = 'dataset',
+              tabPanel('GeneTable',DT::dataTableOutput('GeneTable_logFC'), value = 'GeneTable_logFC'),
+              tabPanel("logFC Bar Plot", plotOutput("GeneBarGraph_logFC"), value='Bar Plot_logFC')
+              
+            )
     ),
     
-    # Show a plot of the generated distribution
-    
-    mainPanel(
-      tabsetPanel(type = "tabs", 
-                  tabPanel("Data Table", dataTableOutput('mytable')),
-                  tabPanel("Histogram",  plotOutput("distPlot")), 
-                  tabPanel("Bar Plot", plotOutput("GeneData"))
-                  
-      )
+    column (width=2, offset = 2, 
+            wellPanel(
+              uiOutput("input_options_PositiveColor"),
+              uiOutput("input_options_NegativeColor")
+            )
     )
-  )))
+    
+  )
+  
+  
+)
