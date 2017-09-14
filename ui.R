a51f088ea086d1785e8b3af6f6d7b69edbd379de
Navya Kompella <navyakrose@gmail.com>
2017-09-14 06:42:10 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,12 +1,13 @@
 library(shiny)
 library(ggplot2)
+library(lattice)
 
 
 fluidPage(
   
   fluidRow(
     
-    column (width = 3,
+    column (width = 2,
             
             wellPanel(
 #***************************************************************************
@@ -18,39 +19,66 @@ fluidPage(
 #***************************************************************************
               uiOutput("input_options_logFC1"),
 #***************************************************************************
-#***************************************************************************        
+#Choices for max  value, logFC
+#***************************************************************************  
+              
               uiOutput("input_options_logFC2"),
+#***************************************************************************
+#Choices for which column to select 
+#***************************************************************************     
               uiOutput("input_options_logFC3"),
               
               
 #***************************************************************************
 #Select Max P.Value
 #***************************************************************************
-              uiOutput("input_options_pvalue1")
+              uiOutput("input_options_pvalue1"),
+              
+#***************************************************************************
+#Options for exporting the data table 
+#***************************************************************************
+              radioButtons("filetype", "Download File type:",
+                           choices = c("csv", "tsv")),
+#***************************************************************************
+#Download data to a file
+#***************************************************************************
+              downloadButton('downloadData', 'Download')
               
               
             )
             
     ),
 #***************************************************************************
-#Output table and bar graph on different tabs of the browser
+#Display all tabs
 #***************************************************************************
     
-    column (width=5, 
+    column (width=6,
             tabsetPanel(
               id = 'dataset',
-              tabPanel('GeneTable',DT::dataTableOutput('GeneTable_logFC'), value = 'GeneTable_logFC'),
-              tabPanel("logFC Bar Plot", plotOutput("GeneBarGraph_logFC"), value='Bar Plot_logFC')
+              tabPanel('GeneTable',dataTableOutput('GeneTable_logFC'), value = 'GeneTable_logFC'),
+              tabPanel("logFC Bar Plot", 
+                       plotOutput("GeneBarGraph_logFC_zoom",
+                                  click = "plot_click",
+                                  dblclick = "plot_dblclick",
+                                  hover = "plot_hover",
+                                  brush = "plot_brush"
+                       ), 
+                       verbatimTextOutput("info"), # display the options picked for zoom
+                       value='Bar Plot_logFC'
+              ),
+              tabPanel("logFC Volcano Plot", plotOutput("GeneVolcanoGraph_logFC"), value='Volcano Plot_logFC')
               
             )
     ),
-    
-    column (width=2, offset = 2, 
+#***************************************************************************
+#color choices for the bar graph
+#***************************************************************************
+    column (width=3,offset=1, 
             wellPanel(
               uiOutput("input_options_PositiveColor"),
               uiOutput("input_options_NegativeColor")
             )
-    )
+    )    
     
   )
   
